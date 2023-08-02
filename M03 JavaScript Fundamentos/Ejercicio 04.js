9b176565d38ab38aa7ff0f152955843654d20358
leydi <leydi.arroyave75@gmail.com>
2023-08-02 14:56:50 -0500

    Solucion ejercicio 4 con Math

--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -22,22 +22,28 @@ function elevar(num, exponent) {
    // Tu código:
    return Math.pow(num, exponent);
 }
+console.log(2.3)
 
 function redondearNumero(num) {
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
+   return Math.round(num);
 }
+console.log(0.49)
 
 function redondearHaciaArriba(num) {
    // Redondea "num" hacia arriba y retórnalo.
    // Tu código:
+   return Math.ceil(num);
 }
+console.log(3.28)
 
 function numeroRandom() {
    // Genera un número al azar entre 0 y 1 y retórnalo.
    // Tu código:
+   return Math.random();
 }
-
+console.log(numeroRandom(1))
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
